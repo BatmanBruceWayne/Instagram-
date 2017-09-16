d058635d24597da6851cbbe8a58db5af418bacde
hieuhtse05008 <goldenrezgun@gmail.com>
2017-09-16 11:33:00 +0700

    login component

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+
+class LoginComponent extends React.Component {
+  render() {
+    return {
+
+    }
+  }
+}
+
+export default LoginComponent;
