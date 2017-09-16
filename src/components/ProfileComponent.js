7af886a106eca298a35879dc087eb5c518f319c1
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-16 16:38:42 +0700

    this is user profile

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -8,29 +8,29 @@ export default class ProfileComponent extends React.Component{
   render(){
     return(
       <div>
-        <div class="wrapper">
-          <div class="page-header page-header-small"
+        <div className="wrapper">
+          <div className="page-header page-header-small"
                style="background-image: url('http://www.hdwallpaperspulse.com/wp-content/uploads/2013/08/10/bridge.jpg');">
-            <div class="filter"></div>
+            <div className="filter"></div>
           </div>
-          <div class="profile-content section">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-md-5 text-center" >
+          <div className="profile-content section">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-5 text-center" >
                   <img src="https://sites.google.com/site/baticlubfcsa/_/rsrc/1476382112707/home/images.jpg?height=236&width=320"
-                       class="rounded-circle" width="150px" height="150px" style="align:right;"/>
+                       className="rounded-circle" width="150px" height="150px" style="align:right;"/>
                 </div>
-                <div class="col-md-6">
-                  <div class="row">
-                    <div class="col-md-5 text-center">
+                <div className="col-md-6">
+                  <div className="row">
+                    <div className="col-md-5 text-center">
                       <h3>Bruce Wayne</h3>
                     </div>
-                    <button class="col-md-5 btn btn-info">Edit Profile</button>
+                    <button className="col-md-5 btn btn-info">Edit Profile</button>
                   </div>
-                  <div class="row">
-                    <div class="col-md-4"><p>4 posts</p></div>
-                    <div class="col-md-4"><p>135 followers</p></div>
-                    <div class="col-md-4"><p>107 following</p></div>
+                  <div className="row">
+                    <div className="col-md-4"><p>4 posts</p></div>
+                    <div className="col-md-4"><p>135 followers</p></div>
+                    <div className="col-md-4"><p>107 following</p></div>
                   </div>
                   <p><b><b>Nguyễn Tiến Tài</b></b></p>
                 </div>
@@ -39,49 +39,49 @@ export default class ProfileComponent extends React.Component{
             </div>
           </div>
 
-          <div class="section latest-offers">
-            <div class="container">
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="card card-product card-plain">
-                    <div class="card-image">
+          <div className="section latest-offers">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="card card-product card-plain">
+                    <div className="card-image">
                       <a href="#paper-kit">
                         <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" class="img-rounded img-responsive"/>
+                             alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
                   </div>
                 </div>
-                <div class="col-md-4">
-                  <div class="card card-product card-plain">
-                    <div class="card-image">
+                <div className="col-md-4">
+                  <div className="card card-product card-plain">
+                    <div className="card-image">
                       <a href="#paper-kit">
                         <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" class="img-rounded img-responsive"/>
+                             alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
                   </div>
                 </div>
-                <div class="col-md-4">
-                  <div class="card card-product card-plain">
-                    <div class="card-image">
+                <div className="col-md-4">
+                  <div className="card card-product card-plain">
+                    <div className="card-image">
                       <a href="#paper-kit">
                         <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" class="img-rounded img-responsive"/>
+                             alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
                   </div>
                 </div>
 
-                <div class="col-md-4">
-                  <div class="card card-product card-plain">
-                    <div class="card-image">
+                <div className="col-md-4">
+                  <div className="card card-product card-plain">
+                    <div className="card-image">
                       <a href="#paper-kit">
                         <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" class="img-rounded img-responsive"/>
+                             alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
