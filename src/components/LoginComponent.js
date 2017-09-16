a57785b5f5e4f693aec54bbb11165c9376710252
hieuhtse05008 <goldenrezgun@gmail.com>
2017-09-16 16:08:29 +0700

    update Login Component - register

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,47 +1,19 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
-import  '../styles/LoginStyle.css'
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
+
 
 class LoginComponent extends React.Component {
   render() {
     return (
     <div className="full-screen register">
       <div className="wrapper">
-        <div className="page-header" style="background-image: url('../assets/img/sections/soroush-karimi.jpg');">
+        <div className="page-header" >
           <div className="filter"></div>
           <div className="container">
             <div className="row">
               <div className="col-md-6 offset-md-1 col-sm-7 col-xs-12">
-                <div className="info info-horizontal">
-                  <div className="icon">
-                    <i className="fa fa-umbrella"></i>
-                  </div>
-                  <div className="description">
-                    <h3> We've got you covered </h3>
-                    <p>Larger, yet dramatically thinner. More powerful, but remarkably power efficient. Everything you need in a single case.</p>
-                  </div>
-                </div>
-                <div className="info info-horizontal">
-                  <div className="icon">
-                    <i className="fa fa-map-signs"></i>
-                  </div>
-                  <div className="description">
-                    <h3> Clear Directions </h3>
-                    <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-                  </div>
-                </div>
-                <div className="info info-horizontal">
-                  <div className="icon">
-                    <i className="fa fa-user-secret"></i>
-                  </div>
-                  <div className="description">
-                    <h3> We value your privacy </h3>
-                    <p>Completely synergize resource taxing relationships via premier niche markets.</p>
-                  </div>
-                </div>
+
+                <img src="https://hootsuite.com/uploads/images/craft/components/media-block/monitor-instagram-effectively.png.png" />
               </div>
               <div className="col-md-4 col-sm-5 col-xs-12">
                 <div className="card card-register">
@@ -49,7 +21,7 @@ class LoginComponent extends React.Component {
                   <div className="social">
                     <button href="#paper-kit" className="btn btn-just-icon btn-facebook"><i className="fa fa-facebook"></i></button>
                     <button href="#paper-kit" className="btn btn-just-icon btn-google"><i className="fa fa-google"></i></button>
-                    <button href="#paper-kit" className="btn btn-just-icon btn-twitter"><i className="fa fa-twitter"></i></button>
+
                   </div>
 
                   <div className="division">
@@ -80,7 +52,6 @@ class LoginComponent extends React.Component {
       </div>
 
     </div>
-
   );
   }
 }
