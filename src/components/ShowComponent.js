bfc6bd278c181e1a3c5f95aa04f0725feebca9aa
hunglhp1998 <hunglhp1998@gmail.com>
2017-09-17 11:31:37 +0700

    Fix ShowComponent

--- src/components/ShowComponent.js
+++ src/components/ShowComponent.js
@@ -1,82 +1,91 @@
 import React from 'react';
 
 
-
 class ShowComponent extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (
-      <div>
-        <div className="blog-2 section section-gray">
-          <div className="container ">
-            <div className="row">
-              <div className="content-center">
-                <div className="col-md-10 offset-md-1">
+        return (
+          <div>
+            <div className="blog-2 section section-gray">
+              <div className="container ">
+                <div className="row">
+                  <div className="content-center">
+                    <div className="col-md-10 offset-md-1">
+                      <div className="row">
+                        <div className="col-md-2 " />
+
+                        <div className="col-md-12">
+                          <center><h1 className="title" style={{color:"mediumseagreen", "font-family" : "TlwgTypewriter"}} >Instagram</h1></center>
+                          <br/><br/> <hr/>
+                          <div className="card card-blog">
 
 
-                  <div className="row">
-                    <div className="col-md-2 ">
-                    </div>
+                            {/*USER        */}
 
 
-                    <div className="col-md-8">
-                      <h1 className="title" style="color:mediumseagreen ; font-family: TlwgTypewriter" align="center">Gallery</h1>
-                    <br/>
-                    <div className="card card-blog">
-
-
-
-
-                      <div className="card-image">
-                        <h4 className="card-title">
-                          <img src="" width="40px" alt="Circle Image" className="img-circle img-no-padding img-responsive" />
-                            <a href="#pablo" id="author" />
-                        </h4><br/><br/>
-
-
-                        <a href="#pablo">
-                          <img id="image"/>
-                        </a>
-                      </div>
+                            <div className="card-image">
+                              <h4 className="card-title" align="left">
+                                <a href="#" id="author" style={{"font-size" : "15px"}}>
+                                  <img src="../clem-onojeghuo-2.jpg" width="35px" alt="Circle Image" className="img-circle img-no-padding img-responsive" />
+                                  Hung
+                                </a>
+                                <p>DESCRIPTION</p>
+                              </h4><br/><br/>
+                            </div>
 
 
 
-                      <div className="card-block">
-                        <div>
-                          <button className="btn btn-just-icon btn-border btn-google">
-                            <i className="fa fa-heart-o" aria-hidden="true" />
-                          </button>
-                          <span className="card-title">
+                            {/*IMAGE      */}
+                            <div>
+                              <a href="#">
+                                <img id="image" src = "../forest-bg.jpg"/>
+                              </a>
+                            </div>
+
+
+                            {/*COMMENT + LIKE ICON  */}
+
+                            <div className="card-block">
+                              <div className="row">
+                                <div className="col-md-2">
+                                  <button className="btn btn-just-icon btn-border btn-google">
+                                    <i className="fa fa-heart-o" aria-hidden="true" />
+                                  </button>
+                                  <span className="card-title">
                                     <a href="#pablo" id="like">23like</a>
                                 </span>
-                          <button className="btn btn-just-icon btn-border btn-github">
-                            <i className="fa fa-comment-o" aria-hidden="true" />
-                          </button>
-                          <br/><br/>
-                          <hr/>
+                                </div>
+                                <div className="col-md-2">
+                                  <button className="btn btn-just-icon btn-border btn-github">
+                                    <i className="fa fa-comment-o" aria-hidden="true" />
+                                  </button>
+                                  <br/><br/>
+                                </div>
+                              </div>
+                              <hr/>
+
+                              {/*COMMENT       */}
+
+                              <input className="form-control mr-sm-2 no-border" placeholder="Add comment" type="text" />
+                            </div>
+                          </div>
                         </div>
-
-
-                        <input className="form-control mr-sm-2 no-border" placeholder="Add comment" type="text" />
                       </div>
+                      <div className="col-md-2" />
                     </div>
                   </div>
                 </div>
-
-
-                <div className="col-md-2" style="height: 300px;">
-                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-  </div>
-    );
+          </div>
+        );
+
+
+
   }
 }
   export default ShowComponent ;
