8d03bb3de29d9798bd9840812230053566cc42b0
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-17 17:20:04 +0700

    this is user profile

--- src/components/PostPhotoComponent.js
+++ src/components/PostPhotoComponent.js
@@ -16,93 +16,37 @@ export default class PostPhotoComponent extends React.Component {
                style={{"background-image" : "url('http://t.wallpaperweb.org/wallpaper/nature/1920x1080/lakeside_v3_wallpaper_pack_by_mpk_1920x1080.jpg')"}}>
             <div className="filter"></div>
           </div>
-          <div className="main">
+          <div className="main center">
             <div className="section section-nude">
               <div className="container">
-                <h3>Share Your Moment With Everybody</h3>
+                <h3 className="text-center">Share Your Moment With Everybody</h3>
                 <form>
-                  <div className="row">
-                    <div className="col-md-5 col-sm-5">
-                      <h6>Product Image</h6>
+                    <div className="center">
+                      <h6>Your Image</h6>
                       <div className="fileinput fileinput-new text-center" data-provides="fileinput">
                         <div className="fileinput-new thumbnail img-no-padding"
                              style={{"max-width": "370px", "max-height": "250px"}}/>
-                        <img src="http://localhost:63342/pk2-pro-html-v2.0.0/assets/img/image_placeholder.jpg" alt="..." height="700.87" width="522.73"/>
+                        <img src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/21751560_2032301003681865_6906246693044740832_n.jpg?oh=b1e7ff4da6ebe46fd43fab67a3dc4dd5&oe=5A54E3B7" alt="..."/>
                       </div>
                       <div className="fileinput-preview fileinput-exists thumbnail img-no-padding"
                            style={{"max-width": "370px", "max-height": "250px"}}></div>
-                      <div>
+                      <div className="center" style={{padding:"30px"}}>
                         <span className="btn btn-outline-default btn-round btn-file"><span className="fileinput-new">Select image</span><span
                           className="fileinput-exists">Change</span><input type="hidden"/><input type="file"
                                                                                                  name="..."/></span>
-                        <a href="#paper-kit" className="btn btn-link btn-danger fileinput-exists"
-                           data-dismiss="fileinput"><i className="fa fa-times"></i> Remove</a>
+                        <button type="button" className="btn btn-danger btn-round">Remove</button>
                       </div>
                     </div>
 
-                    <h6>Tags</h6>
-                    <div id="tags">
-                      <div className="bootstrap-tagsinput"><span className="badge badge badge-success">Minimal<span
-                        data-role="remove"></span></span> <span className="badge badge badge-success"> Light<span
-                        data-role="remove"></span></span> <span className="badge badge badge-success"> New<span
-                        data-role="remove"></span></span> <span className="badge badge badge-success"> Friends<span
-                        data-role="remove"></span></span> <input type="text" placeholder=""/></div>
-                      <input type="text" value="Minimal, Light, New, Friends" data-role="tagsinput"
-                             style={{display: "none"}}/>
-                    </div>
-                    <h6>Categories</h6>
-                    <div id="tags-2">
-                      <div className="bootstrap-tagsinput"><span className="badge badge badge-success">Food<span
-                        data-role="remove"></span></span> <span className="badge badge badge-success"> Drink<span
-                        data-role="remove"></span></span> <input type="text" placeholder=""/></div>
-                      <input type="text" value="Food, Drink" data-role="tagsinput" style={{display: "none"}}/>
-                    </div>
 
-                    <h6>Format <span className="icon-danger">*</span></h6>
-                    <div className="radio">
-                      <input type="radio" name="radio1" id="radio1" value="option1"/>
-                      <label for="radio1">
-                        Digital
-                      </label>
-                    </div>
-                    <div className="radio">
-                      <input type="radio" name="radio1" id="radio2" value="option2" checked=""/>
-                      <label for="radio2">
-                        Print
-                      </label>
-                    </div>
-
-                  </div>
-
-                  <div className="col-md-7 col-sm-7">
-                    <div className="form-group">
-                      <h6>Name <span className="icon-danger">*</span></h6>
-                      <input type="text" className="form-control border-input"
-                             placeholder="enter the product name here..."/>
-                    </div>
-                    <div className="form-group">
-                      <h6>Tagline <span className="icon-danger">*</span></h6>
-                      <input type="text" className="form-control border-input"
-                             placeholder="enter the product tagline here..."/>
-                    </div>
-
+                  <div className="center">
                     <div className="form-group">
                       <h6>Description</h6>
                       <textarea className="form-control textarea-limited"
                                 placeholder="This is a textarea limited to 150 characters."
                                 rows="13" maxlength="150"></textarea>
-                      <h5>
-                        <small><span id="textarea-limited-message" className="pull-right">150 characters left</span>
-                        </small>
-                      </h5>
+                    </div>
 
-                    </div>
-                    <div className="checkbox">
-                      <input id="checkbox1" type="checkbox"/>
-                      <label for="checkbox1">
-                        Display on landing page
-                      </label>
-                    </div>
                   </div>
                 </form>
               </div>
@@ -110,15 +54,12 @@ export default class PostPhotoComponent extends React.Component {
 
 
             <div className="row buttons-row">
-              <div className="col-md-4 col-sm-4">
-                <button className="btn btn-outline-danger btn-block btn-round">Cancel</button>
-              </div>
-              <div className="col-md-4 col-sm-4">
-                <button className="btn btn-outline-primary btn-block btn-round">Save</button>
-              </div>
+              <div className="col-md-4 col-sm-4"></div>
               <div className="col-md-4 col-sm-4">
                 <button className="btn btn-primary btn-block btn-round">Save &amp; Publish</button>
               </div>
+              <div className="col-md-4 col-sm-4"></div>
+
             </div>
 
           </div>
