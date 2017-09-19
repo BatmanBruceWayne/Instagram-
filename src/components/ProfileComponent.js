c2779409aabaec065039a0e451bf6670304c2a03
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-19 21:06:53 +0700

    this is user profile

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import NavbarComponent from './NavbarComponent';
+import {NavLink} from 'react-router-dom';
 
-export default class ProfileComponent extends React.Component{
-  constructor(props, context){
-    super(props,context);
+export default class ProfileComponent extends React.Component {
+  constructor(props, context) {
+    super(props, context);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
+        <NavbarComponent/>
         <div className="wrapper">
           <div className="page-header page-header-small"
-               style="background-image: url('http://www.hdwallpaperspulse.com/wp-content/uploads/2013/08/10/bridge.jpg');">
+               style={{"background-image": "url('http://t.wallpaperweb.org/wallpaper/nature/1920x1080/lakeside_v3_wallpaper_pack_by_mpk_1920x1080.jpg')"}}>
             <div className="filter"></div>
           </div>
           <div className="profile-content section-with-space section-gray">
             <div className="container-fluid">
               <div className="row">
-                <div className="col-md-5 text-center" >
-                  <img src="https://sites.google.com/site/baticlubfcsa/_/rsrc/1476382112707/home/images.jpg?height=236&width=320"
-                       className="rounded-circle" width="150px" height="150px" style="align:right;"/>
+                <div className="col-md-5 text-center">
+                  <img
+                    src="https://sites.google.com/site/baticlubfcsa/_/rsrc/1476382112707/home/images.jpg?height=236&width=320"
+                    className="rounded-circle" width="150px" height="150px" style={{align: "right"}}/>
                 </div>
                 <div className="col-md-6">
                   <div className="row">
@@ -39,23 +43,26 @@ export default class ProfileComponent extends React.Component{
             </div>
           </div>
 
-          <div className="col-md-12 text-center" style="margin: 20px;">
+          <div className="col-md-12 text-center" style={{"margin-top": "30px"}}>
             <button type="button" className="btn btn-danger btn-round">Danger</button>
-            <button className="btn btn-just-icon btn-border btn-youtube">
-              <i className="fa fa-instagram" aria-hidden="true"></i>
-            </button>
+            <NavLink to="/postphoto">
+              <button className="btn btn-just-icon btn-border btn-youtube">
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </button>
+            </NavLink>
             <button type="button" className="btn btn-danger btn-round">Danger</button>
           </div>
 
-          <div className="latest-offers section-with-space section-gray">
+          <div className="latest-offers section-with-space">
             <div className="container">
               <div className="row">
                 <div className="col-md-4">
                   <div className="card card-product card-plain">
                     <div className="card-image">
                       <a href="#paper-kit">
-                        <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" className="img-rounded img-responsive"/>
+                        <img
+                          src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
+                          alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
@@ -65,8 +72,9 @@ export default class ProfileComponent extends React.Component{
                   <div className="card card-product card-plain">
                     <div className="card-image">
                       <a href="#paper-kit">
-                        <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" className="img-rounded img-responsive"/>
+                        <img
+                          src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
+                          alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
@@ -76,8 +84,9 @@ export default class ProfileComponent extends React.Component{
                   <div className="card card-product card-plain">
                     <div className="card-image">
                       <a href="#paper-kit">
-                        <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" className="img-rounded img-responsive"/>
+                        <img
+                          src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
+                          alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
@@ -88,8 +97,9 @@ export default class ProfileComponent extends React.Component{
                   <div className="card card-product card-plain">
                     <div className="card-image">
                       <a href="#paper-kit">
-                        <img src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
-                             alt="Rounded Image" className="img-rounded img-responsive"/>
+                        <img
+                          src="https://i.pinimg.com/736x/73/89/8d/73898d8e27ab3fe6c7764561f91b6813--batman-superman-wonder-woman-batman-artwork.jpg"
+                          alt="Rounded Image" className="img-rounded img-responsive"/>
                       </a>
 
                     </div>
