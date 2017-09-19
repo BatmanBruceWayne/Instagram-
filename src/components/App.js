cbe5901327920fb7081d6a5ddcd31723d0930b34
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-20 00:17:25 +0700

    this is user profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import PropTypes from 'prop-types';
-
-
-// import RegisterComponent from "./RegisterComponent";
-import LoginComponent from "./LoginComponent";
-import ProfileComponent from './ProfileComponent';
-// import NavbarComponent from "./NavbarComponent";
+import {Switch, NavLink} from 'react-router-dom';
+import {Route} from 'react-router';
 import ShowContainer from "../containers/ShowContainer";
+import PostPhotoContainer from "../containers/PostPhotoContainer";
 import NavbarComponent from "./NavbarComponent";
 import PostPhotoComponent from "./PostPhotoComponent";
+import ShowUserComponent from "./ShowUserComponent";
 import ShowComment from "./ShowComment";
-import ShowHeader from "./ShowHeader";
-import ShowImageAndTime from "./ShowImageAndTime";
-
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -22,7 +17,14 @@ import ShowImageAndTime from "./ShowImageAndTime";
 class App extends React.Component {
   render() {
     return (
-<ShowContainer />
+      <div>
+        <div>
+          <NavbarComponent/>
+        </div>
+        <Switch>
+          <Route extra path="/" component={ShowContainer}/>
+        </Switch>
+      </div>
     );
   }
 }
