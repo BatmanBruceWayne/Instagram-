ed37d20b565c091758148de36caabcc07bb4c145
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-21 23:47:50 +0700

    this is user profile

--- src/containers/PostPhotoContainer.js
+++ src/containers/PostPhotoContainer.js
@@ -1,41 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import PostPhotoComponent from '../components/RegisterComponent';
-import * as postPhotoAction from '../actions/postPhotoAction';
+import PostPhotoComponent from '../components/PostPhotoComponent';
 
-class PostPhotoContainer extends React.Component {
-  constructor(props, context) {
+export default class PostPhotoContainer extends React.Component{
+  constructor(props,context){
     super(props, context);
-    this.sendPhotoOnAPI = this.sendPhotoOnAPI.bind(this);
   }
 
-  sendPhotoOnAPI(value) {
-    this.props.postPhotoAction.sendPhotoOnAPI(value);
-  }
-
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <PostPhotoComponent
-          sendPhotoOnAPI={this.sendPhotoOnAPI}
-          listPost={this.props.listPost}/>
+        <PostPhotoComponent/>
       </div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    listPost: state.showReducer.listPost
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    postPhotoAction: bindActionCreators(postPhotoAction, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PostPhotoContainer);
-
