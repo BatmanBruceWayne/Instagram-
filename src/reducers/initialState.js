c9301374082277bdcfb1d3b896c7868144ef885b
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-21 23:48:35 +0700

    this is user profile

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,18 +1,35 @@
 export default {
-  listPost : [
+  listPost: [
     {
-           "id": 1,
-           "description": "gai",
-          "img_url": "http://trainingcolorme.tk/uploads/images/1505636272382a6d7c0e618a7e6caa2f133ddc0fa8.png",
-           "user_id": 20,
-           "created_at": "2017-09-17 08:17:52",
-           "updated_at": "2017-09-17 08:17:52",
-          "deleted_at": null,
-           "like": 0,
-           "user_name": 0
-         }
+      "id": 1,
+      "description": "gai",
+      "img_url": "http://trainingcolorme.tk/uploads/images/1505636272382a6d7c0e618a7e6caa2f133ddc0fa8.png",
+      "user_id": 20,
+      "created_at": "2017-09-17 08:17:52",
+      "updated_at": "2017-09-17 08:17:52",
+      "deleted_at": null,
+      "like": 0,
+      "user_name": 0
+    }
   ],
-  isAdding : true,
+  isAdding: true,
   page_id: 0,
-  token : ""
-  };
+  token: "",
+
+user: {
+  myPhoto: [
+    {
+      "id": 1,
+      "description": "gai",
+      "img_url": "http://trainingcolorme.tk/uploads/images/1505636272382a6d7c0e618a7e6caa2f133ddc0fa8.png",
+      "user_id": 20,
+      "created_at": "2017-09-17 08:17:52",
+      "updated_at": "2017-09-17 08:17:52",
+      "deleted_at": null,
+      "like": 0,
+      "user_name": 0
+    }
+  ]
+}
+
+};
