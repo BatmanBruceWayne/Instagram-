b610eafce16abae4d0e2a18dd118f37dc53aa6ce
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-21 23:53:31 +0700

    this is user profile

--- src/actions/getPhotoToProfileAction.js
+++ src/actions/getPhotoToProfileAction.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
+var user_id = localStorage.getItem("user_id");
 
-export function getPhotoToProfile() {
-  return function () {
+export function getPhotoToProfile(value) {
 
-    axios.get(api,
-      {
-        photo: value.photo,
-        description: value.description
-      })
+  return function (dispatch) {
+    let token = localStorage.getItem("token");
+    console.log(user_id);
+    axios.get('http://api.trainingcolorme.tk/page?token='+ token + '&page_id='+ value )
       .then(function (response) {
-        console.log(response.status);
+        console.log(response.data);
+      dispatch(filterThePhoto(response.data["img_posts"], user_id));
       })
       .catch(function (error) {
-        console.log(error);
+        throw (error);
       });
+
   };
+}
+
+export function filterThePhoto(value, user_id){
+  return({
+    type:'FILTER_THE_PHOTO',
+    value:value,
+    user_id: user_id
+  });
 }
