a1a3b2b0590dc24c7282f6396fb802be03d509ee
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-23 00:53:34 +0700

    batman

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -26,15 +26,15 @@ export default {
 user: {
   myPhoto: [
     {
-      "id": 1,
-      "description": "gai",
-      "img_url": "http://trainingcolorme.tk/uploads/images/1505636272382a6d7c0e618a7e6caa2f133ddc0fa8.png",
-      "user_id": 20,
-      "created_at": "2017-09-17 08:17:52",
-      "updated_at": "2017-09-17 08:17:52",
-      "deleted_at": null,
-      "like": 0,
-      "user_name": 0
+      "id": "",
+      "description": "",
+      "img_url": "",
+      "user_id": "",
+      "created_at": "",
+      "updated_at": "",
+      "deleted_at": "",
+      "like": "",
+      "user_name": ""
     }
   ]
 }
