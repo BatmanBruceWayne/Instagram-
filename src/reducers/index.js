02e400595de5d743cbea33766344f1844da8fdde
BatmanBruceWayne <taideptrai02@gmail.com>
2017-09-23 16:58:19 +0700

    this is user profile

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import showReducer from './showReducer';
 import getPhotoToProfileReducer from './getPhotoToProfileReducer';
-import loginReducer from './loginReducer'
 const rootReducer = combineReducers({
   routing: routerReducer,
   showReducer : showReducer,
-  getPhotoToProfileReducer: getPhotoToProfileReducer,
-  loginReducer : loginReducer
+  getPhotoToProfileReducer: getPhotoToProfileReducer
 });
 
 export default rootReducer;
