619d978889a3ddb213bb8289bc297d5976f33e0b
hieuhtse05008 <31812201+hieuhtse05008@users.noreply.github.com>
2017-09-24 10:27:59 +0700

    Update ProfileComponent.js

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,79 +1,73 @@
-import {Redirect} from 'react-router';
+import {Redirect} from 'react-router'
+import {Switch, NavLink} from 'react-router-dom';
+import {Route} from 'react-router';
 import React from 'react';
+import ProfileContainer from "../containers/ProfileContainer";
+import ProfileSettingComponent from "./ProfileSettingComponent";
 
 export default class ProfileComponent extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.getPhotoToProfile = this.getPhotoToProfile.bind(this);
-    this.directToEdit = this.directToEdit.bind(this);
-    this.directToPost = this.directToPost.bind(this);
-    this.getProfile = this.getProfile.bind(this);
-
-    this.state = {
-      direct: "/profile"
-    };
+    this.getPhotoToProfile=this.getPhotoToProfile.bind(this);
+    this.directToSetting = this.directToSetting.bind(this);
+    this.directToLogin = this.directToLogin.bind(this);
+    this.state ={
+      direct : "/profile"
+    }
   }
-
-  directToEdit() {
-    console.log('direct', this.state);
-    this.setState({direct: "/profile-setting"});
-    console.log('direct', this.state);
+  directToSetting(){
+    this.setState({direct:"/profile-setting"});
   }
-
-  directToPost() {
-    this.setState({direct: "/post_photo"});
+  directToLogin(){
+    localStorage.setItem("token","");
+    localStorage.setItem("userid","");
+    this.setState({direct:"/login"});
   }
-
-  getPhotoToProfile(value) {
+  getPhotoToProfile(value){
     this.props.getPhotoToProfile(value);
-  }
-
-  getProfile(){
-    this.props.getProfile;
   }
 
   render() {
     return (
-      <div onLoad={() => {
-        this.getPhotoToProfile(0);
-        this.getProfile;
-      }}>
+      <div onLoad={() => this.getPhotoToProfile(0)}>
         <div className="wrapper">
           <div className="page-header page-header-small"
                style={{"background-image": "url('http://t.wallpaperweb.org/wallpaper/nature/1920x1080/lakeside_v3_wallpaper_pack_by_mpk_1920x1080.jpg')"}}>
 
             <div className="filter"></div>
           </div>
-
           <div className="profile-content section-with-space section-gray">
             <div className="container-fluid">
               <div className="row">
 
-                <div className="col-md-5 text-center">
+                <div className="col-md-3 text-center">
                   <img
                     src="https://sites.google.com/site/baticlubfcsa/_/rsrc/1476382112707/home/images.jpg?height=236&width=320"
                     className="rounded-circle" width="150px" height="150px" style={{align: 'right'}}/>
 
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-9">
                   <div className="row">
-                    <div className="col-md-5 text-center">
+                    <div className="col-md-4 text-center">
                       <h3>Bruce Wayne</h3>
                     </div>
 
-
-                    <button className="col-md-5 btn btn-success" onClick={this.directToEdit}>
+                    <button className="col-md-3 btn btn-danger" onClick={this.directToSetting}>
                       Edit Profile
                       <Redirect to={this.state.direct}/>
                     </button>
-
+                    <div className="col-md-1"></div>
+                    <button className="col-md-3 btn btn-danger" onClick={this.directToLogin}>
+                      Logout
+                      <Redirect to={this.state.direct}/>
+                    </button>
                   </div>
                   <div className="row">
-                    <div className="col-md-6"><p><b>Gender:  </b>Male</p></div>
-                    <div className="col-md-6"><p><b>Phone:  </b>0941439925</p></div>
+                    <div className="col-md-4"><p>4 posts</p></div>
+                    <div className="col-md-4"><p>135 followers</p></div>
+                    <div className="col-md-4"><p>107 following</p></div>
                   </div>
-                  <p><b><b>Yêu màu hường và ghét sự giả dối </b></b></p>
+                  <p><b><b>Nguyễn Tiến Tài</b></b></p>
                 </div>
               </div>
 
@@ -82,11 +76,13 @@ export default class ProfileComponent extends React.Component {
 
           <div className="col-md-12 text-center" style={{"margin-top": "30px"}}>
 
-            <button type="button" className="btn btn-success btn-round" onClick={this.directToPost}>
-              SHARE YOUR PHOTO WITH US !
-              <Redirect to={this.state.direct}/>
-            </button>
+            <button type="button" className="btn btn-danger btn-round">Danger</button>
 
+              <button className="btn btn-just-icon btn-border btn-youtube" >
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </button>
+
+            <button type="button" className="btn btn-danger btn-round">Danger</button>
           </div>
 
           <div className="latest-offers section-with-space">
@@ -98,11 +94,12 @@ export default class ProfileComponent extends React.Component {
                       <div className="col-md-4">
                         <div className="card card-product card-plain">
                           <div className="card-image">
-                            <a href="#">
+                            <a href="#paper-kit">
                               <img
                                 src={object.img_url}
                                 alt="Rounded Image" className="img-rounded img-responsive"/>
                             </a>
+
                           </div>
                         </div>
                       </div>
@@ -113,6 +110,7 @@ export default class ProfileComponent extends React.Component {
             </div>
           </div>
         </div>
+
       </div>
     );
   }
